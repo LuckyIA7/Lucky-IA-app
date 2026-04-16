6f294701723f4f0138cb579f4d1bab6b7307bba3
LuckyIA7 <katiammbhappy@gmail.com>
2026-04-16 18:23:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.title("🤖 Lucky IA")
+
+produto = st.text_input("🛍️ Qual produto vamos vender?")
+
+if st.button("Criar roteiro"):
+    roteiro = f"""
+    Eu não esperava gostar tanto desse {produto}.
+    Sério, virou meu favorito do dia a dia.
+    Se você gosta de praticidade, vale muito a pena.
+    O link está na sacolinha antes que acabe.
+    """
+
+    st.write("🎬 Roteiro criado:")
+    st.write(roteiro)
